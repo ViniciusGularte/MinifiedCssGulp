4a973a928ba2ebfc6ea98a868b35ef8f1ff62210
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2018-10-05 08:25:59 -0300

    Update Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,12 @@ let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 // Task to minify css using package cleanCSs
 gulp.task('minify-css', () => {
-  // Folder with files to minify
-  return gulp.src('styles/*.css')
-     //I define compatibility with IE 8
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    //I define the destination of the minified files
+     // Folder with files to minify
+     return gulp.src('styles/*.css')
+     //The method pipe() allow you to chain multiple tasks together 
+     //I execute the task to minify the files
+    .pipe(cleanCSS())
+    //I define the destination of the minified files with the method dest
     .pipe(gulp.dest('dist'));
 });
 
